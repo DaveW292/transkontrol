e538a2abaf2b7ce183a5ee9af377f3fb6e8261db
davew292 <davew@tuta.io>
2020-10-16 15:23:30 +0200

    php implemented

--- stopka.php
+++ stopka.php
@@ -1,5 +1,3 @@
-<footer>
 <p>Trans Kontrol &copy; 2020 Wszelkie prawa zastrzeżone.</p>
 <a href="polityka_prywatnosci.php">Polityka prywatności</a>
 <a href="polityka_cookies.php">Polityka cookie</a>
-</footer>
