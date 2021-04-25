17b5556fb4cb87e77e7b3112e72ecfa7691f9ee3
Davesoon <daveson@tuta.io>
2021-04-25 12:37:12 +0200

    project's structure repaired

--- items/footer.php
+++
@@ -1,3 +0,0 @@
-<p>Trans Kontrol &copy; 2021 All rights reserved.</p>
-<a href="privacy-policy">Privacy policy</a>
-<a href="cookies-policy">Cookies policy</a>
--- items/stopka.php
+++
@@ -1,3 +0,0 @@
-<p>Trans Kontrol &copy; 2021 Wszelkie prawa zastrzeżone.</p>
-<a href="polityka-prywatnosci">Polityka prywatności</a>
-<a href="polityka-cookies">Polityka cookies</a>
